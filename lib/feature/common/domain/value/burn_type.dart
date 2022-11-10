607c49f8804ec0af90f52de7e9bbde3407e0944e
bytes7bytes7 <bytes7bytes7@gmail.com>
2022-11-11 00:33:26 +0300

    add MuteType, fix BurnType

--- lib/feature/common/domain/value/burn_type.dart
+++ lib/feature/common/domain/value/burn_type.dart
@@ -1,14 +1,17 @@
 import 'package:equatable/equatable.dart';
 
-abstract class BurnType {
+abstract class BurnType extends Equatable {
   const BurnType();
+
+  @override
+  List<Object?> get props => [];
 }
 
-class NeverBurnType implements BurnType {
+class NeverBurnType extends BurnType {
   const NeverBurnType();
 }
 
-class AfterTimeBurnType extends Equatable implements BurnType {
+class AfterTimeBurnType extends BurnType {
   const AfterTimeBurnType({required this.dateTime});
 
   final DateTime dateTime;
--- lib/feature/common/domain/value/value.dart
+++ lib/feature/common/domain/value/value.dart
@@ -1,5 +1,7 @@
+export 'burn_settings.dart';
 export 'burn_type.dart';
 export 'chat_burn.dart';
 export 'chat_id.dart';
 export 'message_id.dart';
+export 'mute_type.dart';
 export 'user_id.dart';
