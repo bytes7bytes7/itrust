98cc550e11fff271e72c18d1802e755449347db1
bytes7bytes7 <bytes7bytes7@gmail.com>
2022-11-11 23:51:10 +0300

    add BurnSettings

---
+++ lib/feature/common/data/db/tables/burn_settings.dart
@@ -0,0 +1,5 @@
+import 'package:drift/drift.dart';
+
+class BurnSettings extends Table {
+  TextColumn get burnType => text()();
+}
--- lib/feature/common/data/db/tables/tables.dart
+++ lib/feature/common/data/db/tables/tables.dart
@@ -1,2 +1,3 @@
 export 'accounts.dart';
 export 'avatar_urls.dart';
+export 'burn_settings.dart';
