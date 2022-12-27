0c8edcf5dd7f340d573dfae245c2864e24f242d9
bytes7bytes7 <bytes7bytes7@gmail.com>
2022-12-27 23:54:35 +0300

    create NewChatStore

---
+++ lib/feature/new_chat/application/store/new_chat/new_chat_store.dart
@@ -0,0 +1,23 @@
+import 'package:mobx/mobx.dart';
+
+import '../../../../common/application/mixin/mixin.dart';
+import '../../../../common/application/store/search/search_store.dart';
+import '../../../../common/domain/domain.dart';
+
+part 'new_chat_store.g.dart';
+
+class NewChatStore = _NewChatStore with _$NewChatStore;
+
+abstract class _NewChatStore extends SearchStore<User>
+    with Store, Loadable, Errorable {
+  _NewChatStore({
+    required super.searchRepository,
+    required super.isSelectedAlgorithm,
+  });
+
+  @readonly
+  bool _isLoading = false;
+
+  @readonly
+  Object? _error;
+}
