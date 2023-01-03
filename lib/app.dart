a47ebbc63b3a5c2c8cb91aa668026906ddec8c24
bytes7bytes7 <bytes7bytes7@gmail.com>
2023-01-03 16:39:07 +0300

    create MainScreen

--- lib/app.dart
+++ lib/app.dart
@@ -1,24 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
-
-import 'feature/chat_list/presentation/screen/chat_list_screen.dart';
+import 'main_screen.dart';
 import 'theme/theme.dart';
-
-const _navItems = [
-  MapEntry(
-    Icons.home_outlined,
-    Scaffold(),
-  ),
-  MapEntry(
-    Icons.mode_comment_outlined,
-    ChatListScreen(),
-  ),
-  MapEntry(
-    Icons.menu,
-    Scaffold(),
-  ),
-];
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -30,34 +13,7 @@ class App extends StatelessWidget {
       theme: lightTheme,
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
-      home: const _Body(),
-    );
-  }
-}
-
-class _Body extends HookWidget {
-  const _Body();
-
-  @override
-  Widget build(BuildContext context) {
-    final selectedIndex = useState(0);
-
-    return Scaffold(
-      body: _navItems[selectedIndex.value].value,
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: selectedIndex.value,
-        onTap: (index) => selectedIndex.value = index,
-        items: _navItems.map(
-          (e) {
-            return BottomNavigationBarItem(
-              icon: Icon(
-                e.key,
-              ),
-              label: '${selectedIndex.value}',
-            );
-          },
-        ).toList(),
-      ),
+      home: const MainScreen(),
     );
   }
 }
