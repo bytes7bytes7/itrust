f4414d6070fd557dbbdbc603df6c90f5b772de99
bytes7bytes7 <bytes7bytes7@gmail.com>
2023-01-21 12:40:43 +0300

    refactor SyncStore

--- lib/features/common/application/stores/sync_store.dart
+++ lib/features/common/application/stores/sync_store.dart
@@ -1,26 +1,22 @@
 import 'dart:async';
 
 abstract class SyncStore {
-  final _loadingQueue = <int>[];
-  var _currentQueueId = 0;
+  var _queueLength = 0;
 
   Future<void> perform(
     FutureOr<void> Function() callback, {
     required void Function(bool) setIsLoading,
     required void Function() removeError,
   }) async {
-    final id = _currentQueueId++;
-    _loadingQueue.add(id);
+    _queueLength++;
 
     setIsLoading(true);
     removeError();
 
     await callback();
 
-    _loadingQueue.remove(id);
-    if (_loadingQueue.isEmpty) {
-      _currentQueueId = 0;
-
+    _queueLength--;
+    if (_queueLength == 0) {
       setIsLoading(false);
     }
   }
